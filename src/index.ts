0858cf8f5fd1423f466d5068ac162a2730ee38ba
Dinuja Pinto <sri.prabasha@gmail.com>
2024-09-17 14:07:54 +0530

    variables2

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,15 @@ console.log('hello!');
 let age:number = 10;
 let studentName:string='Dinuja Pinto';
 let isActive:boolean=true;
+let myObject:null=null;
+let undefinedData: undefined = undefined;
+let animals:Array<string>=['',''];
+let vehicals:string[]=['','',''];
+let student: {name:string, age:number} = {name:'Dinuja', age:20};
+//-----------
+interface Animal{
+    name:string;
+    breed:string;
+    age:number;
+}
+let dog: Animal={name:'Rex', breed:'Husky', age:2}
