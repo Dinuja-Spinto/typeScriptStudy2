f8629339b532380a67bdfcdc980048984cba3a42
Dinuja Pinto <sri.prabasha@gmail.com>
2024-09-17 14:26:08 +0530

    variables3

--- dist/index.js
+++ dist/index.js
@@ -9,3 +9,10 @@ let animals = ['', ''];
 let vehicals = ['', '', ''];
 let student = { name: 'Dinuja', age: 20 };
 let dog = { name: 'Rex', breed: 'Husky', age: 2 };
+console.log(dog.name);
+function getAgeAfter3Y(age, name) {
+    return age + 3;
+}
+let myAge = getAgeAfter3Y(10);
+console.log(myAge);
+const rate = 1.5;
--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,12 @@ interface Animal{
     age:number;
 }
 let dog: Animal={name:'Rex', breed:'Husky', age:2}
+console.log(dog.name);
+//-------functions
+function getAgeAfter3Y(age:number, name?:string){
+    return age+3;
+}
+let myAge:number = getAgeAfter3Y(10);
+console.log(myAge);
+//-----------
+const rate:number = 1.5;
